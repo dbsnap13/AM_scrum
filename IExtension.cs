e5e6efa7016f54f3504bfab7ec5130e42892edc4
Demir Hajdarevic <demir.hajdarevic@outlook.com>
2016-10-10 23:23:53 +0200

    test

--- IExtension.cs
+++ IExtension.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace TextPoint
 {
-
+    /// <summary>
+    /// DO WE NEED THIS?
+    /// </summary>
     public interface IExtension
     {
         void Initialize(ITextPoint hostApplication);
